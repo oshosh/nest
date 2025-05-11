a940e62411484e8d4f45c41e1a591fa71bc3d4d0
oshosh <dhtpgus7@gmail.com>
2025-05-11 16:18:11 +0900

    feat: db 마이그레이션 & 롤백백, 시딩, 스키마 생성, 테이블 생성 연습

---
+++ src/database/seeds/create-initial-data.ts
@@ -0,0 +1,26 @@
+import { DataSource } from 'typeorm';
+import { Seeder } from 'typeorm-extension';
+import { Channels } from '../../entities/Channels';
+import { Workspaces } from '../../entities/Workspaces';
+
+export default class UserSeeder implements Seeder {
+  public async run(dataSource: DataSource): Promise<any> {
+    const workspacesRepository = dataSource.getRepository(Workspaces);
+    await workspacesRepository.insert([
+      {
+        id: 1,
+        name: 'Sleact',
+        url: 'sleact',
+      },
+    ]);
+    const channelsRepository = dataSource.getRepository(Channels);
+    await channelsRepository.insert([
+      {
+        id: 1,
+        name: '일반',
+        WorkspaceId: 1,
+        private: false,
+      },
+    ]);
+  }
+}
